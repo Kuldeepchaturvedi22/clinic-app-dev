1c00dfd1021a43ac1bc1cabb9c80aa848eaae38d
Kuldeepchaturvedi22 <chaturvedikuldeep40@gmail.com>
2025-12-28 15:10:31 +0530

    Add CI/CD pipeline and update k8s manifests

--- src/main/java/com/example/clinic_appointment_schedulerm/repository/AiChatRepository.java
+++ src/main/java/com/example/clinic_appointment_schedulerm/repository/AiChatRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface AiChatRepository extends JpaRepository<AiChat, Long> {
+public interface         AiChatRepository extends JpaRepository<AiChat, Long> {
     List<AiChat> findByUserEmailOrderByCreatedAtDesc(String userEmail);
     void deleteByUserEmail(String userEmail);
 }
